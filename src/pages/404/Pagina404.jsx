10b2d33097c74cd69cbe3e02c0d7a570611c5596
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-10-21 09:55:54 -0300

    Adição do Caixa no contato 404

--- src/pages/404/Pagina404.jsx
+++ src/pages/404/Pagina404.jsx
@@ -1,3 +1,4 @@
+import Caixa from "../../Components/Caixa/Caixa";
 import estilos from "./Pagina404.module.css";
 import imagem404 from "../../assets/images/404.svg";
 const Pagina404 = () => {
@@ -5,14 +6,14 @@ const Pagina404 = () => {
     <section className={estilos.pagina404}>
       <h2 className={estilos.titulo_secao}>Au, au, au, foi mal!</h2>
 
-      <div id="pagina404">
+      <Caixa id="pagina404">
         <h3>Ops! Página não encontrada!</h3>
         <img
           src={imagem404}
           className={estilos.imagem404}
           alt="Ilustração do cachorro butinho"
         />
-      </div>
+      </Caixa>
     </section>
   );
 };
