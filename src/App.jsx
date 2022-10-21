15c5e874d00b6ead138b3f02199adb7e9d868552
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-10-21 09:04:39 -0300

    Rota 404, página, imagem svg

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home/Home";
 import Produtos from "./pages/Produtos/Produtos";
 import Sobre from "./pages/Sobre/Sobre";
 import Contato from "./pages/Contato/Contato";
+import Pagina404 from "./pages/404/Pagina404";
 
 const App = () => {
   return (
@@ -27,6 +28,10 @@ const App = () => {
 
             <Route path="/contato">
               <Contato />
+            </Route>
+
+            <Route path="*">
+              <Pagina404 />
             </Route>
           </Switch>
         </main>
