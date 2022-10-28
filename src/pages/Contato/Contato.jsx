b29974b19939162c79bff3ef147af1b523b59fc7
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-10-28 11:11:22 -0300

    Recebimento dos dados

--- src/pages/Contato/Contato.jsx
+++ src/pages/Contato/Contato.jsx
@@ -1,15 +1,65 @@
+import serverApi from "../../api/servidor-api";
 import { TextField, Button } from "@mui/material";
+import { useState } from "react";
 import Caixa from "../../Components/Caixa/Caixa";
 import estilos from "./Contato.module.css";
 const Contato = () => {
+  /* Eventos/Funções para captura da digitação nos campos */
+  const inputNome = (event) => {
+    setNome(event.target.value); // Captura da digitação
+  };
+
+  const inputEmail = (event) => {
+    setEmail(event.target.value); // Captura da digitação
+  };
+
+  const inputMensagem = (event) => {
+    setMensagem(event.target.value); // Captura da digitação
+  };
+
+  /* Hooks useState para manipular os estados dos dados do componente */
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [mensagem, setMensagem] = useState("");
+
+  const enviarContato = async (event) => {
+    event.preventDefault();
+
+    const opcoes = {
+      method: "POST",
+      body: JSON.stringify({ nome, email, mensagem }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    };
+    /* Script para envio dos dados a API */
+    try {
+      await fetch(`${serverApi}/contatos`, opcoes);
+      alert("Dados enviados!");
+    } catch (error) {
+      console.log("Deu ruim:" + error.message);
+    }
+    console.log(nome, email, mensagem);
+  };
+
+  // let desabilitado = nome ==="" || email ==="" || mensagem ==="";
+
+  // "Toggle" do botão: caso qualquer uma das variáveis seja undefined, desabilitado se manterá true e com isso o botão ficará desabilitado. Quando toda deixarem de ser undefined, desabilitado se tornará false e com isso o botão será habilitado.
+
+  let desabilitado = !nome || !email || !mensagem;
   return (
     <section>
       <h2 className={estilos.titulo_secao}>Fale Conosco</h2>
 
       <Caixa>
-        <form className={estilos.formulario} method="post">
+        <form
+          onSubmit={enviarContato}
+          className={estilos.formulario}
+          method="post"
+        >
           <div>
             <TextField
+              onChange={inputNome} // Captura da digitação
               label="Nome:"
               variant="outlined"
               fullWidth
@@ -20,6 +70,7 @@ const Contato = () => {
 
           <div>
             <TextField
+              onChange={inputEmail}
               type="email"
               label="email"
               variant="outlined"
@@ -31,6 +82,7 @@ const Contato = () => {
 
           <div>
             <TextField
+              onChange={inputMensagem}
               type="email"
               label="Mensagem"
               variant="outlined"
@@ -43,7 +95,7 @@ const Contato = () => {
           </div>
 
           <div>
-            <Button type="submit" variant="contained">
+            <Button type="submit" variant="contained" disabled={desabilitado}>
               Enviar mensagem
             </Button>
           </div>
