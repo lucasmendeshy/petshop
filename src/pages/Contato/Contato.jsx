bea939f5f8212ea06a785504792aae29e7c2fc46
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-10-28 09:39:06 -0300

    MUI para componentes do formulário

--- src/pages/Contato/Contato.jsx
+++ src/pages/Contato/Contato.jsx
@@ -1,3 +1,4 @@
+import { TextField, Button } from "@mui/material";
 import Caixa from "../../Components/Caixa/Caixa";
 import estilos from "./Contato.module.css";
 const Contato = () => {
@@ -6,9 +7,47 @@ const Contato = () => {
       <h2 className={estilos.titulo_secao}>Fale Conosco</h2>
 
       <Caixa>
-        <p>
-          <i>Em breve teremos um contato com biblioteca com componentes.</i>
-        </p>
+        <form className={estilos.formulario} method="post">
+          <div>
+            <TextField
+              label="Nome:"
+              variant="outlined"
+              fullWidth
+              required
+              helperText="Você deve digitar um nome"
+            />
+          </div>
+
+          <div>
+            <TextField
+              type="email"
+              label="email"
+              variant="outlined"
+              fullWidth
+              required
+              helperText="Informe um e-mail para contato"
+            />
+          </div>
+
+          <div>
+            <TextField
+              type="email"
+              label="Mensagem"
+              variant="outlined"
+              fullWidth
+              required
+              helperText="Fale o que você quiser"
+              multiline
+              rows={6}
+            />
+          </div>
+
+          <div>
+            <Button type="submit" variant="contained">
+              Enviar mensagem
+            </Button>
+          </div>
+        </form>
       </Caixa>
     </section>
   );
