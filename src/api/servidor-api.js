93ec60d98536e4cea5f7cbd811b30ed4ff78d039
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-11-01 09:27:48 -0300

    Configurações para acesso via rede

--- src/api/servidor-api.js
+++ src/api/servidor-api.js
@@ -1,3 +1,4 @@
 const porta = "5000";
-const serverApi = `http://localhost:${porta}`;
+/* const serverApi = `http://localhost:${porta}`; -> Versão localhost */
+const serverApi = `http://10.20.45.39:${porta}`;
 export default serverApi;
