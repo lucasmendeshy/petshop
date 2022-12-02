a7c85dec22018b68c449c625259e964ca040862a
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-12-02 10:26:21 -0300

    Alterando rota

--- src/api/servidor-api.js
+++ src/api/servidor-api.js
@@ -1,4 +1,4 @@
 const porta = "5000";
 /* const serverApi = `http://localhost:${porta}`; -> Versão localhost */
-const serverApi = `http://192.168.1.12:${porta}`;
+const serverApi = `http://10.20.45.39:${porta}`;
 export default serverApi;
