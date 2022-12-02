86cc06e495464f7245b41f99cef672fb0bd0b6f4
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-12-02 11:32:21 -0300

    Ajustes para carregamento dos Posts

--- src/Components/ListaPosts/ListaPosts.jsx
+++ src/Components/ListaPosts/ListaPosts.jsx
@@ -1,48 +1,39 @@
-import { useState, useEffect } from "react"; // Hooks do React
+import { useState, useEffect } from "react";
 import serverApi from "../../api/servidor-api";
 import LoadingDesenho from "../LoadingDesenho/LoadingDesenho.jsx";
 import estilos from "./ListaPosts.module.css";
 import ArtigoPost from "../ArtigoPost/ArtigoPost";
 const ListaPosts = ({ url }) => {
-  /* Iniciamos o state componente com um array vazio,
-  para posteriormente "preechê-lo com os dados vindos da API.
-  Esta atribuição será feita com auxílio do setPosts." */
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     async function getPosts() {
       try {
-        // Solução 1
-        /*  const resposta = await fetch(`${serverApi}/${props.url || "posts"}`); */
+        const resposta = await fetch(`${serverApi}/posts.json`);
+        const dados = await resposta.json(); // OBJETÃO
 
-        // Solução 2
-        /*  const resposta = await fetch(
-          `${serverApi}/${props.url != undefined ? props.url : "posts"}`
-        ); */
-        const resposta = await fetch(`${serverApi}/${url || "posts"} `);
-        const dados = await resposta.json();
-        setPosts(dados);
+        let listaDePosts = [];
+
+        for (const post in dados) {
+          const objetoPost = {
+            id: post, // a chave/string gerado pelo Firebase será como um id
+            titulo: dados[post].titulo,
+            subtitulo: dados[post].subtitulo,
+            descricao: dados[post].descricao,
+            categoria: dados[post].categoria,
+          };
+          listaDePosts.push(objetoPost);
+        }
+        setPosts(listaDePosts);
         setLoading(false);
       } catch (error) {
         console.log("Deu ruim! " + error.message);
       }
     }
     getPosts();
-    /* É necessário indicar a URL como dependência pois ela muda toda vez em que uma categoria é clicada;
-    
-    Desta forma, o useEffect "entende" que ele deve executar novamente as suas ações (neste caso, executar novamente o fetch na API)*/
   }, [url]);
-  /* Sobre o useEffect
-  Este hook visa permitir um maior controle sobre "efeitos colaterais" na execução do componente.
-  
-  Recebe dois parâmetros:
-  1. Função callback com o que será executado.
-  2. Lista de dependências que indicaram ao useEffect quando ele deverá funcionar.
-  
-  - Se não passar a lista (ou seja, se deixar sem os [], useEffect executará toda vez que o componente for redenrizado. Portanto, o callback se torna um loop infinito.)
-  
-  - Se passar a lista vazia (ou seja, deixar o [] vazio), useEffect executará somente no momento que o componente é renderizado a primeira vez evitando o loop infinito no callback.*/
 
   if (loading) {
     return <LoadingDesenho texto="posts..." />;
--- src/api/servidor-api.js
+++ src/api/servidor-api.js
@@ -1,4 +1,4 @@
-const porta = "5000";
-/* const serverApi = `http://localhost:${porta}`; -> Versão localhost */
-const serverApi = `http://10.20.45.39:${porta}`;
+/* const porta = "5000";
+const serverApi = `http://10.20.45.39:${porta}`;  */
+const serverApi = `https://petshop-639c3-default-rtdb.firebaseio.com/`;
 export default serverApi;
